761405ffa8f3776352e72a59b0823610f6783d46
Felipe <lamediatricolor@gmail.com>
2026-03-24 16:49:07 -0500

    ft: excepcion de recurso no encontrado

---
+++ backend/src/main/java/com/hoteleria/backend/common/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.hoteleria.backend.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends BusinessException {
+
+    public NotFoundException(String code, String message) {
+        super(code, message, HttpStatus.NOT_FOUND);
+    }
+}
